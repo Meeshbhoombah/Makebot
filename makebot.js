24da7c898b6594cfd8998372df5275e07d435322
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-17 11:20:34 -0700

    Removed enviromental variables

--- makebot.js
+++ makebot.js
@@ -5,21 +5,11 @@ Makebot.js - a Slackbot for Make School's Product College slack
 
 */
 
-if (!process.env.token) {
-    console.log("Add a token via command line.");
-    process.exit(1);
-}
-
-if (!process.env.uri) {
-    console.log("Add a Firebase URI via command line.");
-    process.exit(1);
-}
-
 var Botkit = require("botkit");
-// TODO - Add Firebase URI
 var os = require("os");
 var cron = require("cron").CronJob;
 var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.uri});
+
 var controller = Botkit.slackbot({
     debug: true,
     require_delivery: true,
