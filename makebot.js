e78404e1c707d6c07ca3dc0d5b0a5e88e6b0beec
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-17 10:42:14 -0700

    Added Firebase URI environment check

--- makebot.js
+++ makebot.js
@@ -6,11 +6,16 @@ Makebot.js - a Slackbot for Make School's Product College slack
 */
 
 if (!process.env.token) {
-    console.log("Specefiy token: ");
+    console.log("Add a token via command line.");
     process.exit(1);
 }
 
-var Botkit = require("node_modules/botkit.js");
+if (!process.env.uri) {
+    console.log("Add a Firebase URI via command line.");
+    process.exit(1);
+}
+
+var Botkit = require("botkit");
 // TODO - Add Firebase URI
 firebaseStorage = require('botkit-storage-firebase')({firebase_uri: '...'}),
     controller = Botkit.slackbot({
@@ -21,18 +26,28 @@ var cron = require("cron").CronJob;
 
 var controller = Botkit.slackbot({
     debug: true,
-    require-delivery: true
+    require-delivery: true,
+    storage: firebaseStorage
 });
 
 var makebot = controller.spawn({
     token: process.env.token
 }).StartRTM();
 
+// Save all Slack users to database
+bot.startRTM(function(err, makebot, payload){
+    if (err) {
+        throw new Error('Unable to connect to Slack channel.');
+    } else {
+        
+    }
+});
+
 // Send sign in message at 9:00 AM, repeat from M to F
 var signInJob = new CronJob({
     cronTime: '00 00 09 * * 1-5',
     onTick: function() {
-        controller.on    
+           
     },
     start: false,
     timeZone: 'America/Los_Angeles'
