9d80310fb2750ff5464eb537ba402dfdea3ecec7
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-17 08:02:35 -0700

    Installed Botkit

--- makebot.js
+++ makebot.js
@@ -3,3 +3,8 @@
 Makebot.js - a simple Slackbot that sends a message with Buttons at 9 AM everyday
 
 */
+
+if (!process.env.token) {
+    console.log("Specefiy token in the environment");
+    process.exit(1);
+}
