d632644e6b04928d7714fc9cfae955cb99e5a807
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-17 13:33:11 -0700

    Prints all users in a Slack team

--- makebot.js
+++ makebot.js
@@ -10,28 +10,34 @@ var SLACK_TOKEN = require('./tokens').SLACK_TOKEN;
 
 // Modules
 var Botkit = require('botkit');
+var mongoStorage = require('botkit-storage-mongo')({mongoUri: MONGO_URI});
+
 var os = require('os');
 var cron = require('cron').CronJob;
-var mongoStorage = require('botkit-storage-mongo')({mongoUri: MONGO_URI});
+
 var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
 var calendar = google.calendar('v3');
 
 var controller = Botkit.slackbot({
     debug: true,
     require_delivery: true,
-    storage: mongoStorage
+    storage: mongoStorage,
+    scopes: ['users']
 });
 
 var makebot = controller.spawn({
     token: SLACK_TOKEN
 }).startRTM();
 
-// Save all Slack users to database
-makebot.startRTM(function(err, makebot, payload){
+// On start, save all users to database
+makebot.startRTM(function(err, makebot, payload) {
     if (err) {
         throw new Error('Unable to connect to Slack channel.');
     } else {
-        
+        makebot.api.users.list({}, function(err, response) {
+            console.log(response);
+        });
     }
 });
 
