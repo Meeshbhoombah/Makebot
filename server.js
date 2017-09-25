93de85d5891875ae4cdc114b4723f096cc930668
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-24 23:12:01 -0500

    Added .rdb backup to .gitignore

--- server.js
+++ server.js
@@ -5,40 +5,44 @@ Makebot.js - a Slackbot for Make School's Product College slack
 
 */
 
-var tokens = require('./tokens').tokens;
+var tokens = require('./tokens');
+var t = new tokens();
+
 var botkit = require('botkit');
 var redisConfig = {};
 var redisStorage = require('botkit-storage-redis')();
 var cron = require('cron').CronJob;
 
 var controller = botkit.slackbot({
+    clientId: tokens.SLACK_CLIENT_ID,
+    clientSecret: tokens.SLACK_CLIENT_SECRET,
+    redirectUri: 'http://localhost:5000',
     storage: redisStorage,
+    scopes: ['bot', 'users.profile:read', 'users:read', 'users:read.email'],
     debug: true,
     require_delivery: true
 });
 
-var makebot = controller.spawn({
-    clientId: tokens.slack.slackClientID,
-    clientSecret: tokens.slack.slackClientSecret,
-    redirectUri: 'http://localhost:5000',
-    scopes: ['bot', 'users.profile:read', 'users:read', 'users:read.email']
-}).startRTM();
-
-// On start, save all users to database using Botkit's storage system
-makebot.startRTM(function(err, makebot, payload) {
+var bot = controller.spawn({
+    // token: tokens.SLACK_BOT_TOKEN
+    token: "xoxb-245078946277-JZXagnS9bdrpWWeZN0lxzD17" 
+}).startRTM(function(err) {
     if (err) {
-        throw new Error(err);
-    } else {
-        makebot.api.users.list({}, function(err, response) {
-            var members = response.members
-
-            for(var i in members) {
-                var user  = {
-                    id: members[i].id,
-                    email: members[i].profile.email
-                }
-            }   
-        })
+        throw new Error('Unable to connect to Slack');
     }
 });
 
+bot.api.users.list({}, function(err, response) {
+    members = response.members;
+
+    for (var i in members){
+        email = members[i].profile.email
+
+        var user  = {
+            id: members[i].id,
+            email: members[i].profile.email
+        };
+
+        controller.storage.users.save(user);
+    }   
+})
