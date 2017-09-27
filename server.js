f4360cd0fd7041fbcafdcb6caa58658c67a2d43d
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-27 07:11:18 -0700

    Added webserver

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // makebot.js
 
+var express    = require('express')
+var bodyParser = require('body-parser')
+var moongose   = require('mongoose')
+
 // Botkit 
-var tokens = require('./tokens');
-var botkit = require('botkit');
+var tokens     = require('./tokens')
+var botkit     = require('botkit')
 
 // Instantiate Botkit Slack controller
 var controller = botkit.slackbot({
@@ -11,10 +15,19 @@ var controller = botkit.slackbot({
     scopes: ['bot', 'users.profile:read', 'users:read', 'users:read.email'],
     debug: true,
     require_delivery: true
-});
+})
 
 // Makebot 🤖
 var makebot = controller.spawn({
     token: tokens.SLACK_BOT_TOKEN
-});
+})
+
+makebot.startRTM(function (err, bot, payload) {
+    if (err) {
+        throw new Error('Unable to connect to Slack')
+    } catch(e) {
+        alert(e)
+        console.log(e)
+    }
+})
 
