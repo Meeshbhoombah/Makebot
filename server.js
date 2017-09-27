91aee390e0626df67a322e2a7847ea2a4c80b850
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-26 20:12:12 -0700

    Added node_modules and other things to .gitignore

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ var tokens = require('./tokens');
 var botkit = require('botkit');
 var redisConfig = {};
 var redisStorage = require('botkit-storage-redis')(redisConfig);
+
 var cron = require('cron').CronJob;
 
+// Set up bot with correct tokens, scope, and redisServer;
 var controller = botkit.slackbot({
     clientId: tokens.SLACK_CLIENT_ID,
     clientSecret: tokens.SLACK_CLIENT_SECRET,
@@ -22,26 +24,6 @@ var controller = botkit.slackbot({
     require_delivery: true
 });
 
-var bot = controller.spawn({
+var makebot = controller.spawn({
     token: tokens.SLACK_BOT_TOKEN
-}).startRTM(function(err) {
-    if (err) {
-        console.log('Unable to connect to Slack');
-    }
 });
-
-bot.api.users.list({}, function(err, response) {
-    members = response.members;
-
-    for (var i in members){
-        email = members[i].profile.email
-
-        var user  = {
-            id: members[i].id,
-            email: members[i].profile.email
-        };
-
-        controller.storage.users.save(user);
-    }   
-});
-
