f2288464fa08bc3171248c4e0c357572d0332cbd
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-27 07:37:30 -0700

    Added signin.js to server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 // makebot.js
 
-var express    = require('express')
-var bodyParser = require('body-parser')
 var moongose   = require('mongoose')
 
-// Botkit 
+// Botkit
 var tokens     = require('./tokens')
 var botkit     = require('botkit')
 
-// Instantiate Botkit Slack controller
+// Instantiate Botkit Slack controller 
 var controller = botkit.slackbot({
     clientId: tokens.SLACK_CLIENT_ID,
     clientSecret: tokens.SLACK_CLIENT_SECRET,
@@ -17,17 +15,15 @@ var controller = botkit.slackbot({
     require_delivery: true
 })
 
-// Makebot 🤖
+// Makebot 🤖 
 var makebot = controller.spawn({
     token: tokens.SLACK_BOT_TOKEN
 })
 
-makebot.startRTM(function (err, bot, payload) {
-    if (err) {
-        throw new Error('Unable to connect to Slack')
-    } catch(e) {
-        alert(e)
-        console.log(e)
-    }
+// Botkit webserver 
+controller.setupWebserver(process.env.port, function(err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver)
 })
+
+require(__dirname + '/skills/signin.js')(makebot)
 
