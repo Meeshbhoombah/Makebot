251d26811c7d6361fb7d1fe2807bb38909306545
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-27 04:24:12 -0700

    Reset server.js

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
-/*
+// makebot.js
 
-Makebot.js - a Slackbot for Make School's Product College slack
-@author Meeshbhoombah
 
-*/
-
+// Botkit 
 var tokens = require('./tokens');
-
-// Bot essentials
 var botkit = require('botkit');
-var redisConfig = {};
-var redisStorage = require('botkit-storage-redis')(redisConfig);
 
 // Instantiate Botkit Slack controller
 var controller = botkit.slackbot({
     clientId: tokens.SLACK_CLIENT_ID,
     clientSecret: tokens.SLACK_CLIENT_SECRET,
-    redirectUri: 'http://localhost:5000',
-    storage: redisStorage,
     scopes: ['bot', 'users.profile:read', 'users:read', 'users:read.email'],
     debug: true,
     require_delivery: true
 });
 
-// The birth of Makebot 🤖
-modeule.exports makebot = controller.spawn({
+// Makebot 🤖
+var makebot = controller.spawn({
     token: tokens.SLACK_BOT_TOKEN
 });
 
