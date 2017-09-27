905b1b7087e97b3a603f08c2516dd5a984f7f085
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-27 10:59:03 -0700

    Created part of variables

--- skills/signin.js
+++ skills/signin.js
@@ -1,1 +1,31 @@
 // signin.js
+
+var CronJob = require('cron').CronJob;
+
+var startOfTheDay = new CronJob({
+  cronTime: '00 00 09  * * 1-5',
+  onTick: function() {
+    
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+
+var signInReminder = new CronJob({
+  cronTime: '00 00 09  * * 1-5',
+  onTick: function() {
+    
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+}
+
+var endOfTheDay = new CronJob({
+    cronTime: '00 00 18  * * 1-5',
+    onTick: function() {
+      
+    },
+    start: false,
+    timeZone: 'America/Los_Angeles'
+}
+
