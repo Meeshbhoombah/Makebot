6a388edaac1a74a45c6aa8bf755d2b67162f1628
Rohan Mishra <rohanmishra2017@gmail.com>
2017-09-30 19:41:19 -0700

    Added keyword reading function

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*
 
-  makebot.js
+    makebot.js
     
 */
 
@@ -15,7 +15,7 @@ var bodyParser  = require('body-parser')
 // Skills ================================================================================
 var signin      = require('skills/signin')
 
-// Makebot 
+// Makebot ===============================================================================
 var controller = botkit.slackbot({
     clientId: tokens.SLACK_CLIENT_ID,
     clientSecret: tokens.SLACK_CLIENT_SECRET,
@@ -24,9 +24,27 @@ var controller = botkit.slackbot({
     require_delivery: true
 })
 
+// The Birth of Makebot 🤖
 var makebot = controller.spawn({
     token: tokens.SLACK_BOT_TOKEN
 })
 
-// Webserver  ============================================================================
+// Makebot Express Server ================================================================
+controller.hears('another_keyword','direct_message,direct_mention',function(bot,message) {
+  var reply_with_attachments = {
+    'username': 'My bot' ,
+    'text': 'This is a pre-text',
+    'attachments': [
+      {
+        'fallback': 'To be useful, I need you to invite me in a channel.',
+        'title': 'How can I help you?',
+        'text': 'To be useful, I need you to invite me in a channel ',
+        'color': '#7CD197'
+      }
+    ],
+    'icon_url': 'http://lorempixel.com/48/48'
+    }
+
+  bot.reply(message, reply_with_attachments);
+});
 
