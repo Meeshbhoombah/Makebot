0c498a92d288f5ddfa256c4dd58119f31d62d48b
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-06 03:04:01 -0700

    Got rid of Javascript version of bot 😶. Never writing in that language again

--- deprecated/tokens.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    SLACK_CLIENT_ID: "245306317191.244267989568",
-    SLACK_CLIENT_SECRET: "dca203908b26b9265b86b5b7cc5f9b94",
-    SLACK_BOT_TOKEN: "xoxb-245078946277-1vndPnF8X7Gh5jfroZAUJ578",
-    MONGO_URI: "mongodb://localhost:27017/db",
-    DASHBOT_KEY: "7df6Z9a2ffUb2QlCP0Ypw8MJCseyDWW60ejdUETR",
-    GOOGLE_CLIENT_ID: "489153125590-hnl2s9knmlpbguoh0vcgbq3id5vakj26.apps.googleusercontent.com"
-}
--- models.py
+++ models.py
@@ -7,9 +7,14 @@ Models for the MongoDB database
 from mongoengine import *
 connect('userdb')
 
+'''
+User
+- Slack ID
+- Name
+- Email
+'''
 class User(Document):
     slack_id = StringField(required = True) 
     name = StringField(required = True)
     email = EmailField(required = True)
-    meta = {'allow_inheritance': True}
 
