b8926ee3e73fe51c6656aa25ae72285a2173b25e
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-06 04:14:02 -0700

    Generated emojis

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ Makebot
 
 A Slack bot written in Flask with Mongo and python Slackclient
 '''
-
 from models import User
 from mongoengine import *
 from slackclient import SlackClient
@@ -25,7 +24,6 @@ members = team_data['members']
 # loop thru all members and save them to the database
 for member in members:
     # don't save slackbot or other bots
-
     if member['is_bot'] == False and member['id'] != 'USLACKBOT':
         member_id = member['id']
         
@@ -34,9 +32,38 @@ for member in members:
         member_email = member_profile['email']
         
         new_member = User(slack_id = member_id, name = member_name, email = member_email)
-        new_member.save()
+        # TODO - uncomment:  new_member.save()
 
 '''
-SKILLS ============================================================
+TESTING
 '''
+import time
+import random
+
+# from mongoengine import *
+# from slackclient import SlackClient
+import schedule
+import emoji
+
+general_emoji_list = [':smile:', ':shit:',':alien:', ':heart:', ':robot:', ':ocean:', ':octocat:',':ghost:']
+this_day_emoji_list = []
+
+def generate_emojis():
+    for i in range(4):
+        random_emoji = random.choice(general_emoji_list)
+
+        if random_emoji not in this_day_emoji_list:
+            this_day_emoji_list.append(random_emoji)
+        else:
+            i--
+            pass
+
+def sign_in_open():
+    generate_emojis()
+
+    for user in User.objects:
+        send_sign_in_message(user.slack_id)
+    
+
+sign_in_open()
 
