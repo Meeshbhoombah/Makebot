ceb875e8c21d4cde2ddf17d66926ad6af30b2ca6
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-06 02:58:13 -0700

    Created database of users

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
+'''
+Makebot
+~~~~~~~
 
-# bot.py
+A Slack bot written in Flask with Mongo and python Slackclient
+'''
 
+from models import User
+from mongoengine import *
+from slackclient import SlackClient
 import os
 
-from flask import Flask
-from mongoengine import *
-
-from slackclient import SlackClient
-
+# bot config
 BOT_NAME     = 'makebot'
 BOT_ID       = os.environ.get('BOT_ID')
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-app = Flask(__name__)
-
 '''
-DATABASE CONFIG ===================================================
+DATABASE SETUP =====================================================
 '''
 # get user data from Slack
 team_data = slack_client.api_call('users.list')
@@ -24,9 +25,18 @@ members = team_data['members']
 # loop thru all members and save them to the database
 for member in members:
     # don't save slackbot or other bots
+
     if member['is_bot'] == False and member['id'] != 'USLACKBOT':
-        print(member['id'])
+        member_id = member['id']
+        
         member_profile = member['profile']
-        print(member_profile['real_name'])
-        print(member_profile['email'])
-      
+        member_name = member_profile['real_name']
+        member_email = member_profile['email']
+        
+        new_member = User(slack_id = member_id, name = member_name, email = member_email)
+        new_member.save()
+
+'''
+SKILLS ============================================================
+'''
+
