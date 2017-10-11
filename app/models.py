29c0407e6a92be27c8aafb0bc806563a3e797e4e
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-11 16:52:47 -0700

    removed __init__

--- app/models.py
+++ app/models.py
@@ -7,10 +7,10 @@ Licence: GPLv3
 from app import db
 
 class ModelExample(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(250))
-	content = db.Column(db.Text)
-	date = db.Column(db.DateTime)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250))
+    content = db.Column(db.Text)
+    date = db.Column(db.DateTime)
 
 
 class User(db.Model):
--- deprecated/__init__.py
+++
@@ -1,1 +0,0 @@
-from .makebot import app
