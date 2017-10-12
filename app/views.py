945c89ff3df865f0c2fbd0477fb1e9ac5fad4477
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-11 21:20:37 -0700

    Started database setup

--- app/views.py
+++ app/views.py
@@ -5,18 +5,28 @@ Licence: GPLv3
 """
 
 from flask import url_for, redirect, render_template, flash, g, session
-from flask.ext.login import login_user, logout_user, current_user, login_required
+from slackclient import SlackClient
 from app import app, lm
-from forms import ExampleForm, LoginForm
+
+# -*- Models -*-
+from models import Emoji
 from models import User
 
+
+# create new instructor
+
+
+# home about page for Malebot 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# emoji page for the app
 @app.route('/emoji/')
 def posts():
+
     return render_template('emoji.py')
+
 
 
