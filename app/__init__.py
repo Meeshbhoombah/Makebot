bc7ee4836e9efbaf0d4509d4ad487c40dfb185c8
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-17 11:22:23 -0800

    Reset proj

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8
-from flask import Flask
-
-""" CONFIG """
-app = Flask(__name__)
-from . import views
-
--- app/views.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import app
-
-@app.route
-def index():
-    return 'Hello World!'
-
