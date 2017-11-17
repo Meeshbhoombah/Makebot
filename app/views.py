86cb7cfb44def7f648703f9dd181918f9193e6ba
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-17 13:01:07 -0800

    Started signin view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+# -*- encoding: utf-8 -*-
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
