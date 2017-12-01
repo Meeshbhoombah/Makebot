ccc0b59e9b18c46bf6d4c2be234f99127395909d
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-30 23:43:27 -0800

    Restructuring v1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # -*- encoding: utf-8 -*-
-""" app.py
+""" 
+app.py
 
-Routing layer for makerbot
+Creates the Makebot server to extend the functionality of the Makebot Slack bot.
 """
 
 import json
@@ -10,36 +11,11 @@ from flask import Flask, request, make_response, render_template
 
 app =  Flask(__name__)
 
-@app.route('/makebot/listenting', methods = ['GET', 'POST'])
-def listener():
-    """
-    Listens for incoming events from Slack and uses Makebot's built-in
-    event-handler helper function to route events to the right plugins.
-    """
-    event = json.loads(request.data)
-
-    # Slack URL Verification
-    if 'challenge' in event:
-        # response must include challenge request token
-        response_token   = event['challenge']
-        response_code    = 200
-        response_headers = { 'content_type': 'application/json' }
-
-        # response to challenge request
-        return make_response(response_token, response_code, response_headers)
-
-    # TODO - Verify Incoming Request
-
-    # Handler
-    if 'event' in event:
-        event_type = event['event']['type']
-        #makebot.handle_event(event_type, event)
-
 # WEBSERVER
 @app.route('/dashboard/signin-emoji')
 def daily_sign_in():
     return 'Sign-in'
 
 if __name__ == '__main__':
-    app.run(debug = True, use_reloader = True) # reloader = dev
+    app.run(debug = True, use_reloader = True) # TODO - remove use_reloader before installing
 
